9dcc19c39d66df271769b22be02c33e6fce4d84d
Chris Kim <chriskihyun@hotmail.com>
2018-06-19 02:04:42 +0000

    Enumeration with Candidates exercise

--- filters.rb
+++ filters.rb
@@ -1,16 +1,76 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
 
+
 def find(id)
-  # Your code Here
+  #Your code Here
+  @candidates.each do |candidate|
+    if candidate[:id] == id
+      pp candidate
+    else
+      nil
+    end
+  end
 end
 
 def experienced?(candidate)
   # Your code Here
+  if candidate[:years_of_experience] >= 2
+     true
+  else
+     false
+  end
 end
 
-def qualified_candidates(candidates)
-  # Your code Here
+def qualified_candidates?(candidates)
+  @candidates.each do |candidate|
+    if experienced?(candidate) && have_100_github_points?(candidate) && know_Ruby_Python?(candidate) && applied_last_15_days?(candidate) && over_age_17?(candidate)
+      pp candidate
+    end
+  end
 end
 
-# More methods will go below
+def have_100_github_points?(candidate)
+  if candidate[:github_points] >= 100
+     true
+  else
+     false
+  end
+end
+
+def know_Ruby_Python?(candidate)
+  if candidate[:languages].include?('Ruby') || candidate[:languages].include?('Python')
+    true
+  else
+    false
+  end
+end
+
+def applied_last_15_days?(candidate)
+  if candidate[:date_applied] <= 15.days.ago.to_date
+    true
+  else
+    false
+  end
+end
+
+def over_age_17?(candidate)
+  if candidate[:age] > 17
+    true
+  else
+    false
+  end
+end
+
+def ordered_by_qualifications?(candidates)
+  candidates.sort_by {|candidate| [candidate[:years_of_experience], candidate[:github_points]]}.reverse
+end
+# {
+#     id: 5,
+#     years_of_experience: 4,
+#     github_points: 293,
+#     languages: ['C', 'Ruby', 'Python', 'Clojure'],
+#     date_applied: 5.days.ago.to_date,
+#     age: 26
+#   },
+
--- main.rb
+++ main.rb
@@ -9,4 +9,29 @@ require './filters'
 
 # binding.pry
 
-pp @candidates
+# pp @candidates
+
+# find(7)
+
+# puts experienced?(@candidates[0])
+# puts experienced?(@candidates[1])
+# puts experienced?(@candidates[2])
+# puts experienced?(@candidates[3])
+# puts experienced?(@candidates[4])
+# puts experienced?(@candidates[5])
+# puts experienced?(@candidates[6])
+# puts have_100_github_points?(@candidates[4])
+# puts know_Ruby_Python?(@candidates[0])
+# puts know_Ruby_Python?(@candidates[1])
+# puts Applied_last_15_days?(@candidate[0])
+# puts applied_last_15_days?(@candidates[0])
+# puts applied_last_15_days?(@candidates[1])
+# puts applied_last_15_days?(@candidates[2])
+# puts over_age_17?(@candidates[6])
+# puts @candidates[1][:date_applied]
+
+# qualified_candidates?(@candidates)
+
+pp ordered_by_qualifications?(@candidates)
+
+
